99ae87da9106a22218836b5052fbe5d629632b03
4ohblack-dev <4oh.black@gmail.com>
2026-06-02 14:44:03 +0900

    model test

--- yolo_model_test.py
+++ yolo_model_test.py
@@ -3,23 +3,23 @@ import matplotlib.pyplot as plt
 import os
 from ultralytics import YOLO
 from pathlib import Path
-
-def main():
-
-    base_dir = Path(__file__).resolve().parent
-    model_path = base_dir/"best.pt"
-    model = YOLO(str(model_path))
-
-    test_imgs_path = base_dir/"test_img"
-    if not os.path.exists(test_imgs_path):
-        print("error")
-
-    print("successed")
+import numpy as np
 
 
+base_dir = Path(__file__).resolve().parent
+model_path = base_dir/"best.pt"
 
+model = YOLO(str(model_path))
+test_imgs_path = base_dir/"test_img"
 
+if not os.path.exists(test_imgs_path):
+    print("error")
 
-
-if __name__ == "__main__":
-    main()
+results = model.predict(test_imgs_path,save=True,project="test",name="")
+print(len(results))
+test_result = results[3]
+testimg = cv2.cvtColor(test_result.plot(),cv2.COLOR_BGR2RGB)
+plt.figure(figsize=(20,20))
+plt.imshow(testimg)
+plt.axis("off")
+plt.show()
