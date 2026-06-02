0ecb09040a6855b9975d2abbc5aa1969a14429c9
4ohblack-dev <4oh.black@gmail.com>
2026-06-02 15:08:38 +0900

    camera connected

--- yolo_model_test.py
+++ yolo_model_test.py
@@ -6,20 +6,41 @@ from pathlib import Path
 import numpy as np
 
 
-base_dir = Path(__file__).resolve().parent
-model_path = base_dir/"best.pt"
+#base_dir = Path(__file__).resolve().parent
+#model_path = base_dir/"best.pt"
+#model = YOLO(str(model_path))
 
-model = YOLO(str(model_path))
-test_imgs_path = base_dir/"test_img"
+cap = cv2.VideoCapture(1,cv2.CAP_DSHOW)
+cap.set(cv2.CAP_PROP_FPS,30)
 
-if not os.path.exists(test_imgs_path):
-    print("error")
+if not cap.isOpened():
+    print("cannot find cam")
+    exit()
 
-results = model.predict(test_imgs_path,save=True,project="test",name="")
-print(len(results))
-test_result = results[3]
-testimg = cv2.cvtColor(test_result.plot(),cv2.COLOR_BGR2RGB)
-plt.figure(figsize=(20,20))
-plt.imshow(testimg)
-plt.axis("off")
-plt.show()
+while True:
+    ret,frame = cap.read()
+    if not ret == True:
+        break
+
+    #img = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
+    cv2.imshow("webcam",frame)
+
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
+
+cap.release()
+cv2.destroyAllWindows
+
+#test_imgs_path = base_dir/"test_img"
+#
+#if not os.path.exists(test_imgs_path):
+#    print("error")
+#
+#results = model.predict(test_imgs_path,save=True,project="test",name="")
+#print(len(results))
+#test_result = results[3]
+#testimg = cv2.cvtColor(test_result.plot(),cv2.COLOR_BGR2RGB)
+#plt.figure(figsize=(20,20))
+#plt.imshow(testimg)
+#plt.axis("off")
+#plt.show()
