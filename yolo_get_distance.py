9082dad2a624d3595115417ad46dc8c75bf3b4b9
4ohblack-dev <4oh.black@gmail.com>
2026-06-04 13:53:56 +0900

    fixed

--- yolo_get_distance.py
+++ yolo_get_distance.py
@@ -7,7 +7,7 @@ import numpy as np
 def main():
     F_length = 100.0
     real_width = 10.0
-    model = YOLO("best_seg.onnx")
+    model = YOLO("best_seg.onnx",task="segment")
     cap= cv2.VideoCapture(1,cv2.CAP_DSHOW)
     cap.set(cv2.CAP_PROP_FPS,30)
     cap.set(cv2.CAP_PROP_FRAME_WIDTH,1280)
@@ -18,31 +18,33 @@ def main():
         if not ret:
             break
         else:
-            results = model.predict(frame,stream=True,verbose=False,conf=0.6)
-            for r in results:
-                masks=r.masks
-                boxes=r.boxes
+            results = model.predict(frame,stream=False,verbose=False,conf=0.4)
+            r=results[0]
+            masks=r.masks
+            boxes=r.boxes
 
-                if masks is not None:
-                    for box,mask in zip(boxes,masks):
-                            mask_data = ((mask.data[0].cpu().numpy())*255).astype(np.uint8)
-                            mask_data = cv2.resize(mask_data,(frame.shape[1],frame.shape[0]))
-                            
+            if hasattr(r, 'masks') and masks is not None and hasattr(r, 'boxes') and boxes is not None:
+                masks_data = masks.data.cpu().numpy()
+                for box,mask in zip(boxes,masks_data):
+                    
+                    mask_data = (mask*255).astype(np.uint8)
+                    mask_data = cv2.resize(mask_data,(frame.shape[1],frame.shape[0]), interpolation=cv2.INTER_NEAREST)
+                    
+                    contours, _ = cv2.findContours(mask_data,cv2.RETR_EXTERNAL,  cv2.CHAIN_APPROX_SIMPLE)
+                    if contours:
+                         largest_contour= max(contours,key=cv2.contourArea)
+                         (_,_),radius = cv2.minEnclosingCircle(largest_contour)
+                         pixel_parameter = 2*radius
 
-                            contours, _ = cv2.findContours(mask_data,cv2.RETR_EXTERNAL,  cv2.CHAIN_APPROX_SIMPLE)
-                            if contours:
-                                 largest_contour= max(contours,key=cv2.contourArea)
-                                 (_,_),radius = cv2.minEnclosingCircle(largest_contour)
-                                 pixel_parameter = 2*radius
-
-                                 if pixel_parameter>0:
-                                    distance = F_length*real_width/pixel_parameter
-                                    cv2.drawContours(frame, [largest_contour], -1, (0, 255, 0), 2)
-                                    x1, y1, x2, y2 = map(int, box.xyxy[0])
-                                    text = f"Dist: {distance:.1f} cm"
-                                    cv2.putText(frame, text, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 
-                                    0.6, (0, 0, 255), 2)
-                cv2.imshow("Focal Length Calibrator (1280x720)", frame)
+                         if pixel_parameter>0:
+                            distance = F_length*real_width/pixel_parameter
+                            cv2.drawContours(frame, [largest_contour], -1, (0, 255, 0), 2)
+                            x1, y1, x2, y2 = map(int, box.xyxy[0])
+                            text = f"Dist: {distance:.1f} cm"
+                            cv2.putText(frame, text, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 
+                            0.6, (0, 0, 255), 2)
+            
+            cv2.imshow("Focal Length Calibrator (1280x720)", frame)
             if cv2.waitKey(1) & 0xFF == ord("q"):
                 break
     
