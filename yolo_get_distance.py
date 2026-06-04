27643b1ca3cd0778377dc1c354565a326edf9962
4ohblack-dev <4oh.black@gmail.com>
2026-06-04 12:51:05 +0900

    tested

--- yolo_get_distance.py
+++ yolo_get_distance.py
@@ -13,31 +13,41 @@ def main():
     cap.set(cv2.CAP_PROP_FRAME_WIDTH,1280)
     cap.set(cv2.CAP_PROP_FRAME_HEIGHT,720)
 
-    while cap.Opened():
+    while cap.isOpened():
         ret, frame = cap.read()
         if not ret:
             break
         else:
-            results = model.predict(frame,stream=True,varbose=False,conf=0.6)
+            results = model.predict(frame,stream=True,verbose=False,conf=0.6)
             for r in results:
                 masks=r.masks
                 boxes=r.boxes
 
                 if masks is not None:
                     for box,mask in zip(boxes,masks):
-                        class_id= int(box.cls[0])
+                            mask_data = ((mask.data[0].cpu().numpy())*255).astype(np.uint8)
+                            mask_data = cv2.resize(mask_data,(frame.shape[1],frame.shape[0]))
+                            
 
+                            contours, _ = cv2.findContours(mask_data,cv2.RETR_EXTERNAL,  cv2.CHAIN_APPROX_SIMPLE)
+                            if contours:
+                                 largest_contour= max(contours,key=cv2.contourArea)
+                                 (_,_),radius = cv2.minEnclosingCircle(largest_contour)
+                                 pixel_parameter = 2*radius
+
+                                 if pixel_parameter>0:
+                                    distance = F_length*real_width/pixel_parameter
+                                    cv2.drawContours(frame, [largest_contour], -1, (0, 255, 0), 2)
+                                    x1, y1, x2, y2 = map(int, box.xyxy[0])
+                                    text = f"Dist: {distance:.1f} cm"
+                                    cv2.putText(frame, text, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 
+                                    0.6, (0, 0, 255), 2)
+                cv2.imshow("Focal Length Calibrator (1280x720)", frame)
+            if cv2.waitKey(1) & 0xFF == ord("q"):
+                break
     
-        
-         
+    cap.release()
+    cv2.destroyAllWindows()
              
-             
-
-
-    
-
-
-
-
 if __name__ == "__main__":
     main()
